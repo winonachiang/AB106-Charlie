fa91cadaa1c78fa6d2c0d977c5291b4c47c47f6c
Chiang, Chih-Yu <c29559810@hotmail.com>
2017-07-24 17:27:23 +0800

    Update sina(old).py

--- sina(old).py
+++ sina(old).py
@@ -16,7 +16,7 @@ def crawlPage(url):
     contents = soup.select('#artiBlock > p')
     contents_dict = {}
     for page in range(len(contents)):
-        contents_dict[page+1] = contents[page].text.replace('\n', '').replace('\'', '').replace('\"', '').strip()
+        contents_dict[page+1] = contents[page].text.replace('\n', '').replace('\'', '').replace('\"', '').replace("\xa0","").replace("\u200b", "").replace("\xa0", "").replace("\xa06", "").replace("\u3000", "").strip()
     #article = contents.text.replace('\n', '').replace('\'', '').replace('\"', '').strip()
     articles_dict['contents'] = contents_dict #article
     articles_dict['url'] = url.replace("\n", "")
@@ -24,15 +24,17 @@ def crawlPage(url):
     print(articles_dict)
 
     return articles_dict
-
+#public static void main([args] Strings){
+#   system.out.println("Hello World!")
+#}
 
 f = open('C:\\Users\\CF_NB.CF.000\\uncrawledurls\\sina(old).txt','rt')
 lines = f.readlines()#.decode("utf-8")
 #print(lines).replace("\n", " ")
 for i in lines:
-    urlopen(i)#.decode("utf-8")
+    #urlopen(i) #Do not use this function when crawling.
     crawlPage(i)
 
 	#print(i)
 
-#crawlPage('https://disp.cc/b/733-74wY')
+#crawlPage('https://www.ct.org.tw/1244979')
