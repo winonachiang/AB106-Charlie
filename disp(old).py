5296dd57ccef1871862fc4ad535f9e20ae3ed34f
Chiang, Chih-Yu <c29559810@hotmail.com>
2017-08-02 14:31:03 +0800

    Update disp(old).py

--- disp(old).py
+++ disp(old).py
@@ -1,12 +1,15 @@
+import jieba
+import jieba.analyse
 import requests
 from urllib.request import urlopen
 import urllib.parse
 from bs4 import BeautifulSoup
-import pickle
-import json
+import time
 import datetime
+import pickle
+datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-def crawlPage(url):
+def getDisp(url):
 
     res = requests.get(url)
     res.encoding = 'utf-8'
@@ -16,23 +19,32 @@ def crawlPage(url):
     contents = soup.select('#text > div')
     contents_dict = {}
     for page in range(len(contents)):
-        contents_dict[page+1] = contents[page].text.replace('\n', '').replace('\'', '').replace('\"', '').strip()
-    #article = contents.text.replace('\n', '').replace('\'', '').replace('\"', '').strip()
-    articles_dict['contents'] = contents_dict #article
+        contents_dict[page+1] = contents[page].text.replace('\n', '').replace('\'', '').replace('\"', '').replace("\xa0", "").replace("\u3000", "").replace("\xa06", "").replace("\xa03", "").replace("\xa02", "").replace("\xa01", "").strip()   
+    unixtime = int(time.time())  #標記時間戳(系統時間)   
+    hash_object = hashlib.md5(url.encode())
+    MD5code = hash_object.hexdigest()  #將url轉成MD5並且存取下來
+    jieba.set_dictionary('./dict.txt.big.txt')  #結巴斷詞字庫
+    words = jieba.cut(contents_dict, cut_all=False) 
+    words = jieba.analyse.set_stop_words('./stopwords.txt')  
+    words = jieba.analyse.extract_tags(str(contents_dict),10)  #利用jieba.analyse來計算文本TF-IDF關鍵詞
+    
+    
     articles_dict['url'] = url.replace("\n", "")
-    articles_dict['source'] = 'https://disp.cc/'
-    print(articles_dict)
+    articles_dict['MD5'] = MD5code
+    articles_dict['unix_time'] = unixtime
+    articles_dict['datetime'] = datetime
+    articles_dict["TF-IDF words list"] = str(words).replace("[", "").replace("]", "")
+    #articles_dict['contents'] = contents_dict 
+    #print(articles_dict)
 
     return articles_dict
 
 
-f = open('C:\\Users\\CF_NB.CF.000\\uncrawledurls\\disp(old).txt','rt')
-lines = f.readlines()#.decode("utf-8")
+#f = open('C:\\Users\\CF_NB.CF.000\\uncrawledurls\\disp(old).txt','rt')
+#lines = f.readlines()#.decode("utf-8")
 #print(lines).replace("\n", " ")
-for i in lines:
-    urlopen(i)#.decode("utf-8")
-    crawlPage(i)
+#for i in lines:
+    #urlopen(i)#.decode("utf-8")
+    #getDisp(i)
 
-	#print(i)
-
-#crawlPage('https://disp.cc/b/733-74wY')
+getDisp('https://disp.cc/b/733-74wY')
