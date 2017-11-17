7b0f6dda5fe3d63e09f495303eef1b9cc0405c67
Chiang, Chih-Yu <c29559810@hotmail.com>
2017-11-17 11:33:24 +0800

    Update urlcrawler(auto).py

--- urlcrawler(auto).py
+++ urlcrawler(auto).py
@@ -1,4 +1,5 @@
-import newspaper
+#這支程式用以判斷由DSP送來的url是否已存在在人群分類DB裏頭，並決定是否要將url送入url list DB待爬
+# import newspaper
 import hashlib
 import time
 import pymongo
@@ -6,62 +7,39 @@ from pymongo import MongoClient
 try:
     client = MongoClient('127.0.0.1', 27017)
     db = client['admin'] #連結到mongoDB裡的database
-    collection = db['crawledurlsTest'] #連結到該database的table
-    collection2 = db['crawledurlsTest2']
+    collection = db['crawledurlsTest'] #dsp送過來的url
+    collection2 = db['crawledurlsTest2'] # url list DB
+    collection3 = db['temp2'] #人群標籤DB
     print("Connects to MongoDB successfully!!")
 except:
     print('Cannot connect to MongoDB server!!')
 
-def encodeUrl(url):
-    hash_object = hashlib.md5(url.encode())
-    MD5code = hash_object.hexdigest()
-    url = {}
-    url['url'] = url
-    url['_id'] = MD5code
-    print(url)
-    return url
-
-urls = []
-documents = collection.find()
+#def encodeUrl(url):
+#    hash_object = hashlib.md5(url.encode())
+#    MD5code = hash_object.hexdigest()
+#    url = {}
+#    url['url'] = url
+#    url['_id'] = MD5code
+#    print(url)
+#    return url
+#encodeUrl("http://eznewlife.com/150535/以前都考100分？兒子竟意外翻出「爸爸的國中考卷」猛一看…網友都要跪惹！")
+"""urls = []
+documents = collection3.find()
 for document in documents:
     mdid = document["_id"]
     urls.append(mdid)
 #encodeUrl("https://www.soft4fun.net/tech/news/htc-u11-plus.htm")
 #利用無窮迴圈，先設定每2分鐘訪問要抓url的網站一次，之後可以再做調整。
-while True: #time.sleep(120)                                 #後面參數要取消掉重複爬url的功能
-    newurl = newspaper.build("https://www.soft4fun.net/",  memoize_articles=False)
-    for article in newurl.articles:
-        try:
-            urlcompiled = encodeUrl(article.url)
-            print(urlcompiled)
-            if urlcompiled["_id"] not in urls:
-                collection2.insert_one(urlcompiled)
-                print("url sent to list DB")
-            else:
-                print("url already exists")
-        except:
-            print("Unable to insert url!")
-            pass
-        print(article.url)
-    time.sleep(120)
-
-#url儲存資料庫DB-------------------------------------------------------------------------------------
-"""newurl = {}
-url = "http://garyliutw.blogspot.tw/2014/05/mongodb-nosql.html"
-hash_object = hashlib.md5(url.encode())
-MD5code = hash_object.hexdigest()
-newurl["_id"] = MD5code
-newurl["url"] = url
-#print(newurl["_id"])
-
-urls = []
-documents = collection.find()
-for document in documents:
-    mdid = document["_id"]
-    urls.append(mdid)
-
-if newurl["_id"] not in urls:
-    collection2.insert_one(newurl)
-    print("url is sent to list DB")
-else:
-    print("url already exists")"""
+while True: #time.sleep(120)
+    documents = collection.find()
+    for document in documents:
+        if document["_id"] not in urls:
+            try:
+                collection2.insert_one(document)
+            except:
+                pass
+                print("nope")
+            print("url sent to list DB")
+        else:
+            print("url already exists")
+    time.sleep(120)"""
