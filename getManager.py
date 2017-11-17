1ce2a254896499c5ac73def0793d6c0ee86cd802
Chiang, Chih-Yu <c29559810@hotmail.com>
2017-11-17 11:34:00 +0800

    Update getManager.py

--- getManager.py
+++ getManager.py
@@ -1,27 +1,29 @@
+#寫個log把程式執行的動作記錄起來
 #取得與mongoDB的連線
 import jieba
 import jieba.analyse
 from newspaper import Article
 import time
-#import hashlib
+import hashlib
 import newspaper
 import re
-re_words = re.compile("[\u4e00-\u9fa5]+")
-#import pymongo
-#from pymongo import MongoClient
-"""try:
+#re_words = re.compile("[\u4e00-\u9fa5]+")
+import pymongo
+from pymongo import MongoClient
+try:
     client = MongoClient('127.0.0.1', 27017)
     db = client['admin'] #連結到mongoDB裡的database
-    collection = db['crawledurls3'] #連結到該database的table
+    collection = db['temp'] #連結到該database的table
+    #collection2 = db['temp2']
     print("Connects to MongoDB successfully!!")
 except:
-    print('Cannot connect to MongoDB server!')"""
-
+    print('Cannot connect to MongoDB server!')
+#
 #---------------------------------------------我是分隔線----------------------------------------------
 
 #Get contents of articles and insert them into mongoDB
 #Main program:
-def getArticle(url):
+"""def getArticle(url):
     art_dict = {} #把文章內容存成一個 dictionary
     art = Article(url, language = "zh")
     try:
@@ -32,39 +34,61 @@ def getArticle(url):
         pass
     art_text = art.text.replace('\n', '').replace('\'', '').replace('\"', '').replace("\xa0", "").replace("\u3000", "").replace("\xa06", "").replace("\xa03", "").replace("\xa02", "").replace("\xa01", "").strip()
     art_url = art.url.replace("\n", "")   #原始的url
-    #unixtime = int(time.time())  #標記時間戳(系統時間)
-    #hash_object = hashlib.md5(art_url.encode())
-    #MD5code = hash_object.hexdigest()  #將url轉成MD5並且存取下來
+    unixtime = int(time.time())  #標記時間戳(系統時間)
+    hash_object = hashlib.md5(art_url.encode())
+    MD5code = hash_object.hexdigest()  #將url轉成MD5並且存取下來
     #match = re.findall(re_words, art_text)
-    jieba.set_dictionary('C:\\Users\\defaultuser0\dict.txt')  #結巴斷詞字庫
+    jieba.set_dictionary('C:\\Users\\defaultuser0\\dict.txt')  #結巴斷詞字庫
     words = jieba.cut(art_text, cut_all=False)
-    words = jieba.analyse.set_stop_words('C:\\Users\\defaultuser0\\stopwords.txt')
-    words = jieba.analyse.extract_tags(str(art_text),15)  #利用jieba.analyse來計算文本TF-IDF關鍵詞
+    words = jieba.analyse.set_stop_words('C:\\Users\\defaultuser0\\stopwords(ken).txt')
+    words = jieba.analyse.extract_tags(str(re.sub("[0-9]", "", art_text)),15)  #利用jieba.analyse來計算文本TF-IDF關鍵詞
     art_dict["content"] = art_text         #switch to match
     art_dict["url"] = art_url
     #art_dict["source"] = "technews"
-    #art_dict["unix_time"] = unixtime
-    #art_dict["MD5"] = MD5code
+    art_dict["unix_time"] = unixtime
+    art_dict["_id"] = MD5code
     art_dict["TF-IDF words list"] = str(words).replace("[", "").replace("]", "")
-    #art_dict["EnglishKwds"] = str(set(re.findall('[a-zA-Z]+', art_text))).replace("{", "").replace("}", "")
     print(art_dict)
-    return art_dict
+    return art_dict #記得換回art_dict"""
 #
-getArticle("http://www.chinatimes.com/newspapers/20170928000125-260204")
+#getArticle("http://www.autonet.com.tw/cgi-bin/view.cgi?/news/2017/10/b7100253.ti+a2+a3+a4+a5+b1+/news/2017/10/b7100253+/news/2017/10/20+b3+d6+c1+c2+c3+e1+e2+e3+e5+f1")
 #-----------------------------------------------------------------------------------------------------
-
-"""f = open('C:\\Users\\defaultuser0\\uncrawledurls(k)\\technews(k).txt','rt',encoding="utf8")
-line = f.readlines()
-for i in line:
+#
+#documents = collection2.find()
+"""for document in documents:
     try:
-        art_dict = getArticle(i)
-        if len(art_dict['content']) < 65:
-            print("Article is too short.")
-            pass
+        keywords = getArticle(document['url'])
+        if len(keywords['content']) > 130:
+            collection2.insert_one(keywords)
+            #collection.delete_one({"unixtime":{"$exists":False}})
         else:
-            db.crawledurls3.insert_one(art_dict)
+            print("This article is too short.")
+            pass
+        #print(keywords)
     except:
-        print("There's no articles!!")
+        print("http 404 not found!")
         pass"""
-
+#for document in documents:
+    #print(document["_id"])
+    #print(document["TF-IDF words list"].replace("'","").replace(",",""))
 #-----------------------------------------------------------------------------------------------------
+"""import csv
+with open("C:\\Users\\USER\\Documents\\ID(test).csv") as csvfile:
+    spamreader = csv.DictReader(csvfile)
+    #topics = {}
+    for row in spamreader:
+        topic = row['topics']
+        print(topic)
+        collection.find_one_and_update({'topics':{'$exists':False}},{"$set":{"topics":topic}}, upsert=True)"""
+#
+"""from collections import Counter
+with open("C:\\Users\\USER\\Desktop\\charlie-stopwords.txt", "r", encoding="utf8") as f:
+    words = f.readlines()
+
+word_cnt = Counter()
+for word in words:
+    if word in word_cnt:
+        word_cnt[word] += 1
+    else:
+        word_cnt[word] = 1
+print(word_cnt.most_common())"""
